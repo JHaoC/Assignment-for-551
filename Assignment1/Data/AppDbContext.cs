49dae6f06c55b1d9e3366ed17099fa1a824043ab
Junhao Chen <juchen@microdesk.com>
2020-10-31 18:17:54 -0400

    Assignment 2:
    Create students model. and enable EF,  and create a database and  Update database by EF at local.

---
+++ Assignment1/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Assignment1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment1.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+       
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
+
